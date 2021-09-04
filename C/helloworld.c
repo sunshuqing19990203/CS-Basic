a6788e00fdd3667b747d79a48730ac2f46031f31
sunshuqing19990203 <3356014655@qq.com>
2021-09-04 20:55:15 +0800

    C 语言：配置VScode环境

---
+++ C/helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(int argc, char **argv){
+    printf("Hello,World.\n");
+    int i = 0;
+    i = 1;
+    i = 2;
+    scanf("%d",&i);
+    printf("i = %d\n", i);
+    return 0;
+}
