d35397fc99955d8b75f6abc50e9bf0251b091397
sunshuqing19990203 <3356014655@qq.com>
2021-09-05 18:35:16 +0800

    动态分配内存

---
+++ C/malloc.c
@@ -0,0 +1,27 @@
+// 动态内存分配
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //malloc申请一个数组的空间
+    // int n = 10;
+    // int *a;
+    // a = (int *)malloc(sizeof(n * sizeof(int)));
+    // for(int i = 0; i < n; i++){
+    //     a[i] = i;
+    //     printf("a[%d] = %d\n",i,a[i]);
+    // }
+    // free(a);
+    // return 0;
+
+
+    // 测试可以分配多少空间
+    // void *p;
+    // int cnt = 0;
+    // while(p = malloc(100*1024*1024)){
+    //     cnt++;
+    // }
+    // printf("%d00MB\n",cnt);
+    // return 0;
+
+}
